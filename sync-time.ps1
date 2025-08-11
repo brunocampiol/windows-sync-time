c5b84a72817f31bc7a2fd4c0b820b76883050f5f
bruno <1672440+brunocampiol@users.noreply.github.com>
2025-08-11 19:36:34 -0300

    try catch removal

--- sync-time.ps1
+++ sync-time.ps1
@@ -43,53 +43,39 @@ function Get-NtpTime {
         [string]$Server,
         [switch]$WarmupOnly
     )
-    
-    try {
-        $endpoint = [System.Net.Dns]::GetHostEntry($Server).AddressList[0]
-        $socket = New-Object System.Net.Sockets.Socket(
-            [System.Net.Sockets.AddressFamily]::InterNetwork,
-            [System.Net.Sockets.SocketType]::Dgram,
-            [System.Net.Sockets.ProtocolType]::Udp
-        )
-        $socket.ReceiveTimeout = 999
-        $socket.SendTimeout = 999
+
+    $endpoint = [System.Net.Dns]::GetHostEntry($Server).AddressList[0]
+    $socket = New-Object System.Net.Sockets.Socket(
+        [System.Net.Sockets.AddressFamily]::InterNetwork,
+        [System.Net.Sockets.SocketType]::Dgram,
+        [System.Net.Sockets.ProtocolType]::Udp
+    )
+    $socket.ReceiveTimeout = 999
+    $socket.SendTimeout = 999
         
-        # Construct NTP request
-        $ntpData = [byte[]]::new(48)
-        $ntpData[0] = 0x1B  # NTP client mode (version 3)
+    # Construct NTP request
+    $ntpData = [byte[]]::new(48)
+    $ntpData[0] = 0x1B  # NTP client mode (version 3)
         
-        # Send/receive data
-        $socket.Connect($endpoint, 123)
-        [void]$socket.Send($ntpData)
-        [void]$socket.Receive($ntpData)
+    # Send/receive data
+    $socket.Connect($endpoint, 123)
+    [void]$socket.Send($ntpData)
+    [void]$socket.Receive($ntpData)
         
-        if ($WarmupOnly) {
-            return
-        }
-        
-        # Convert timestamp (big-endian)
-        $intPart = [BitConverter]::ToUInt32($ntpData[43..40], 0)
-        $fracPart = [BitConverter]::ToUInt32($ntpData[47..44], 0)
-        
-        # Calculate milliseconds since 1900
-        $ms = ($intPart * 1000) + ($fracPart * 1000 / 4294967296)
-        
-        # Convert to UTC DateTime
-        $ntpEpoch = New-Object System.DateTime(1900, 1, 1, 0, 0, 0, [System.DateTimeKind]::Utc)
-        return $ntpEpoch.AddMilliseconds($ms)
+    if ($WarmupOnly) {
+        return
     }
-    catch {
-        if (-not $WarmupOnly) {
-            Write-Error "NTP communication failed: $_"
-            exit 1
-        }
-    }
-    finally {
-        if ($socket) { 
-            $socket.Close() 
-            $socket.Dispose()
-        }
-    }
+        
+    # Convert timestamp (big-endian)
+    $intPart = [BitConverter]::ToUInt32($ntpData[43..40], 0)
+    $fracPart = [BitConverter]::ToUInt32($ntpData[47..44], 0)
+        
+    # Calculate milliseconds since 1900
+    $ms = ($intPart * 1000) + ($fracPart * 1000 / 4294967296)
+        
+    # Convert to UTC DateTime
+    $ntpEpoch = New-Object System.DateTime(1900, 1, 1, 0, 0, 0, [System.DateTimeKind]::Utc)
+    return $ntpEpoch.AddMilliseconds($ms)
 }
 
 # Perform 5 warmup requests to stabilize the network route
@@ -97,16 +83,16 @@ Write-Host "Warming up NTP connection..."
 1..5 | ForEach-Object {
     Get-NtpTime -Server $NTPServer -WarmupOnly
     Start-Sleep -Milliseconds 100
-    Write-Host "Warmup request $_ completed" -Verbose
+    Write-Host "Warmup request $_ completed"
 }
 
 # Get the actual time after warmup
 $utcTime = Get-NtpTime -Server $NTPServer
-Write-Host "Warmed up UTC time: $($utcTime.ToString('o'))" -Verbose
+Write-Host "Warmed up UTC time: $($utcTime.ToString('o'))"
 
 # Convert to UTC-3 (hardcoded offset)
 $localTime = $utcTime.AddHours(-3)
-Write-Host "Adjusted local time (UTC-3): $($localTime.ToString('o'))" -Verbose
+Write-Host "Adjusted local time (UTC-3): $($localTime.ToString('o'))"
 
 # Update system time
 try {
